ec88512e3bb45e0175881f3141dca7588e3b1919
lukedever <luke_44@163.com>
2020-08-21 23:11:28 +0800

    fix routes comment

--- resources/js/libs/routes.js
+++ resources/js/libs/routes.js
@@ -4,8 +4,8 @@ Vue.use(VueRouter)
 
 const routes = [
     { path: '/', name: 'home', component: () => import('../pages/Index.vue') },
-    { path: '/login', name: 'login', component: () => import('../pages/auth/Login.vue'), meta: { guest: true } },
     //auth-route-start
+    { path: '/login', name: 'login', component: () => import('../pages/auth/Login.vue'), meta: { guest: true } },
     { path: '/register', name: 'register', component: () => import('../pages/auth/Register.vue'), meta: { guest: true } },
     { path: '/forget', name: 'forget', component: () => import('../pages/auth/Forget.vue'), meta: { guest: true } },
     { path: '/password/reset', name: 'reset', component: () => import('../pages/auth/Reset.vue'), meta: { guest: true } },
